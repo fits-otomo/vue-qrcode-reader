e40f059989aff6d93e67552b6fa9dc97dcf7a447
fits-otomo <otomo.si@fits-inc.jp>
2019-10-23 16:46:56 +0900

    device find return true

--- src/misc/camera.js
+++ src/misc/camera.js
@@ -50,16 +50,16 @@ export default async function(constraints, videoEl) {
     streamApiShimApplied = true;
   }
 
-  let camDeviceId = '';
+  // let camDeviceId = '';
   await navigator.mediaDevices.enumerateDevices().then(function (devices) {
     devices.some(function (device) {
-      // if (device.label === WebCam) {
-      //   console.log(device.label);
-      //   camDeviceId = device.deviceId;
-      //   return true;
-      // }
-    })
-  })
+      if (device.label === WebCam) {
+        console.log(device.label);
+        // camDeviceId = device.deviceId;
+        return true;
+      }
+    });
+  });
   // constraints.base.deviceId = camDeviceId;
 
   const stream = await navigator.mediaDevices.getUserMedia(constraints);
